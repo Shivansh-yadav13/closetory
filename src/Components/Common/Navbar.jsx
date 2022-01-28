50f5019180a64331a48788097a28aa0f062b3f0e
Shivansh-yadav13 <yadavshivansh@gmail.com>
2022-01-28 10:50:19 +0530

    init

---
+++ src/Components/Common/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>Casetory</ul>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Shop</li>
+          <li>About Us</li>
+        </ul>
+      </div>
+      <div>
+        <ul>User</ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/Components/Home/Categories.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Categories.css"
+
+const Categories = () => {
+  return (
+    <>
+      <h1 className="heading">Categories</h1>
+      <div className="cat-div">
+        <div className="cat-box">
+          Cat 1
+        </div>
+        <div className="cat-box">
+          Cat 1
+        </div>
+        <div className="cat-box">
+          Cat 1
+        </div>
+        <div className="cat-box">
+          Cat 1
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Categories;
